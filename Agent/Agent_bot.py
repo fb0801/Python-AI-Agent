82a5e8fdbfbe537aacdda7a6adefe6ddcfb55e66
fb0801 <fbhatti0801@gmail.com>
2025-12-09 17:15:11 +0000

    agentbot 1 code

--- Agent/Agent_bot.py
+++ Agent/Agent_bot.py
@@ -9,4 +9,19 @@ load_dotenv()
 class AgentState(TypedDict):
     messages: List[HumanMessage]
 
-    llm = ChatOpenAI(model = "gpt-4o")
+llm = ChatOpenAI(model = "gpt-4o")
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state['messages'])
+    print(f'\nAI: {response.content}')
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+
+user_input = input("Enter: ")
+agent.invoke({"messages": [HumanMessage(content=user_input)]})
