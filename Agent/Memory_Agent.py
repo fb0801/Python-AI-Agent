92c27c07f74df6a6abf8efe6951a314a9535d5b4
fb0801 <fbhatti0801@gmail.com>
2025-12-09 17:48:42 +0000

    agentbot 2 code

---
+++ Agent/Memory_Agent.py
@@ -0,0 +1,19 @@
+from typing import TypedDict, List, Union
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class AgentState(TypedDict):
+    messages: List[Union[HumanMessage, AIMessage]]
+
+llm = ChatOpenAI(model="gpt-4o")
+
+def process(state: AgentState) -> AgentState:
+    """Node to solve request"""
+    response = llm.invoke(state['messages'])
+
+    state["messages"].append(AIMessage(content=response.content))
