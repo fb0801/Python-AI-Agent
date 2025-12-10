d0a2dffcafcc803e61e9a9fca190e679c0ff79a4
fb0801 <fbhatti0801@gmail.com>
2025-12-10 17:41:15 +0000

    agentbot 2 code 1.2

--- Agent/Memory_Agent.py
+++ Agent/Memory_Agent.py
@@ -18,6 +18,7 @@ def process(state: AgentState) -> AgentState:
 
     state["messages"].append(AIMessage(content=response.content))
     print(f"\nAI: {response.content}")
+    print("CURRENT STATE: ", state["messages"])
 
     return state
 
@@ -38,3 +39,15 @@ while user_input != "exit":
     conversation_history = result["messages"]
 
     user_input = input("Enter: ")
+
+
+with open("logging.txt", "w") as file:
+    file.write("Your conversation Log:\n")
+    for message in conversation_history:
+        if isinstance(message, HumanMessage):
+            file.write(f"You: {message.content}\n")
+        elif isinstance(message, AIMessage):
+            file.write(f"AI: {message.content}\n\n")
+    file.write("End of Conversation")
+
+print("Conversation saved to logging.txt")
