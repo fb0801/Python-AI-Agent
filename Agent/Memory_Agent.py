908d433cbf125708ff0e2101041e45a0b804b7e7
fb0801 <fbhatti0801@gmail.com>
2025-12-10 17:15:52 +0000

    agentbot 2 code 1.2

--- Agent/Memory_Agent.py
+++ Agent/Memory_Agent.py
@@ -17,3 +17,24 @@ def process(state: AgentState) -> AgentState:
     response = llm.invoke(state['messages'])
 
     state["messages"].append(AIMessage(content=response.content))
+    print(f"\nAI: {response.content}")
+
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+conversation_history = []
+user_input = input('Enter: ')
+while user_input != "exit":
+    conversation_history.append(HumanMessage(content = user_input))
+
+    result = agent.invoke({"messages": conversation_history})
+
+    print(result["messages"])
+    conversation_history = result["messages"]
+
+    user_input = input("Enter: ")
