b467f7d1133a30c0f721fd1224eeb4a9f3945c4b
fb0801 <fbhatti0801@gmail.com>
2025-12-23 17:07:12 +0000

    agentbot 4.1

--- Agent/Drafter.py
+++ Agent/Drafter.py
@@ -23,3 +23,31 @@ def update(content: str) -> str:
     global document_content
     document_content = content
     return f"Document has been updated successfully! The current content is:\n{document_content}"
+
+
+@tool
+def save(filename: str) -> str:
+    """save current doc to txt file
+
+    args:
+    filename : name of file
+    """
+    global document_content
+
+    if not filename.endswith('.txt'):
+        filename = f"{filename}.txt"
+
+
+    try:
+        with open(filename, 'w') as file:
+            file.write(document_content)
+            print(f"Document has been saved to: {filename}")
+            return f"Document has been saved successfully to '{filename}'."
+        
+    except Exception as e:
+        return f"Error saving document: {str(e)}"
+    
+tools = [update, save]
+
+model = ChatOpenAI(model = 'gpt-4o').bind_tools(tools)
+
